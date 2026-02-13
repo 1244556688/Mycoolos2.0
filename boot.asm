826cfdce9c226f15bccce96717efb8868c0a2414
1244556688 <o396g4h2@gmail.com>
2026-02-13 23:07:47 +0800

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,29 +1,35 @@
-; boot.asm
+; boot.asm 修正版
 global start
-extern kmain            ; 呼叫 C 語言的入口函數
+extern kmain
+global load_gdt
 
 section .text
 bits 32
 
 start:
-    ; 1. 設定堆疊指標 (Stack Pointer)
     mov esp, stack_top
-
-    ; 2. 處理 Multiboot 資訊
-    ; GRUB 會把 Multiboot Info 的記憶體位址放在 EBX 暫存器
     push ebx 
-
-    ; 3. 跳轉到 C 語言核心
     call kmain
-
-    ; 4. 如果核心意外返回，讓 CPU 進入無窮迴圈
     cli
-.hang:
-    hlt
+.hang: hlt
     jmp .hang
+
+; 載入 GDT 的函式
+load_gdt:
+    mov eax, [esp + 4]
+    lgdt [eax]
+    ; 刷新段暫存器
+    jmp 0x08:.flush
+.flush:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    ret
 
 section .bss
 align 16
-stack_bottom:
-    resb 16384 ; 16 KB Kernel Stack
+stack_bottom: resb 16384
 stack_top:
